1f12b68c547351793977091ebfa08e1036c1d419
PacoV3 <franj.vents@gmail.com>
2021-03-24 17:48:05 -0600

    Class for ProductionLine and Gaphs

--- one_server.py
+++ one_server.py
@@ -1,48 +1,53 @@
-import numpy as np
+from numpy.random import exponential, normal
+import matplotlib.pyplot as plt 
 import simpy
 
-time_per_car = []
-def car(env, name, bcs, driving_time, charge_duration):
-    # Simulate driving to the BCS
-    yield env.timeout(driving_time)
-    # Request one of its charging spots
-    # print('%s arriving at %d' % (name, env.now))
+class ProductionLine:
+    def __init__(self, env, bcs):
+        self.env = env
+        self.bcs = bcs
+        self.inspection_times = []
 
-    with bcs.request() as req:
-        yield req
+    def piece(self, name, arriving_time, inspect_time):
+        # Simulate the piece arriving to the BCS
+        yield self.env.timeout(arriving_time)
+        # Request one of its inspection spots
+        # print('%s arriving at %d' % (name, self.env.now))
+        with self.bcs.request() as req:
+            yield req
+            # Charge the battery
+            # print('%s starting to inspect at %s' % (name, self.env.now))
+            yield self.env.timeout(inspect_time)
+            # print('%s leaving the bcs at %s' % (name, self.env.now))
+            self.inspection_times.append(self.env.now - arriving_time)
+        
+    def build_simulation(self, max_time):
+        time = 0
+        count = 0
+        while True:
+            exp_time = exponential(5)
+            norm_time = normal(4,0.5)
+            if time + exp_time + norm_time > max_time:
+                break
+            self.env.process(self.piece('Piece %d' % count, time + exp_time, norm_time))
+            time += exp_time
+            count += 1
 
-        # Charge the battery
-        # print('%s starting to charge at %s' % (name, env.now))
-        yield env.timeout(charge_duration)
-        # print('%s leaving the bcs at %s' % (name, env.now))
-        time_per_car.append((name, env.now - driving_time))
-
+servers = 1
+max_time = 10000
 env = simpy.Environment()
-bcs = simpy.Resource(env, capacity=1)
-
-time = 0
-count = 0
-max_time = 200
-while True:
-    exp_time = int(np.random.exponential(5))
-    norm_time = int(np.random.normal(4,0.5))
-    if time + exp_time + norm_time > max_time:
-        break
-    # revisar si me pase del 40
-    env.process(car(env, 'Car %d' % count, bcs, time + exp_time, norm_time))
-    time += exp_time
-    count += 1
-
-# for i in range(4):
-#     arrive_time = np.random.exponential(5)
-#     charge_time = np.random.normal(4,0.5)
-#     env.process(car(env, 'Car %d' % i, bcs, arrive_time, charge_time))
-#     print(arrive_time, charge_time)
-
+bcs = simpy.Resource(env,capacity = servers)
+p_line = ProductionLine(env, bcs)
+p_line.build_simulation(max_time = max_time)
 env.run()
-sum_of_times = 0
-for car, time in time_per_car:
-    sum_of_times += time
-    # print(f"Average time: {sum_of_times / len(time_per_car)}")
-    print(f"Time for {car}: {time}")
 
+times = p_line.inspection_times
+x = range(len(times))
+y = [sum(times[:index + 1]) / (index + 1) for index in range(len(times))]
+
+plt.title("Simulation behavior") 
+plt.xlabel("Piece number") 
+plt.ylabel("Average time in inspection") 
+plt.plot(x, y, color ="red") 
+plt.legend([f"servers = {servers}, max time = {max_time}"], loc ="upper right")
+plt.savefig('graphs/one_server.png')
