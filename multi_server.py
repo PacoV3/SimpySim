53e3fc66b17e14e427b61a00a12dcede82ea1f46
PacoV3 <franj.vents@gmail.com>
2021-04-14 14:59:57 -0500

    Revision rapida de simpy
    Co-authored-by: HectorJ2810 <HectorJ2810@users.noreply.github.com>

--- multi_server.py
+++ multi_server.py
@@ -1,40 +1,41 @@
 from numpy.random import exponential, normal
+from numpy import average
 import matplotlib.pyplot as plt
 import simpy
 
 
 class ProductionLine:
-    def __init__(self, env, bcs, l, mean, sd):
-        self.env = env
-        self.bcs = bcs
-        self.l = l
-        self.mean = mean
-        self.sd = sd
-        self.inspection_times = []
+    def __init__(self, env, bcs):
+        self.env = env # Lo que hace que funcione simpy
+        self.bcs = bcs # Recurso para procesar las piezas
+        self.inspection_times = [] # Tiempo en el sistema para todas las piezas
 
     def piece(self, name, arriving_time, inspect_time):
-        # Simulate the piece arriving to the BCS
         yield self.env.timeout(arriving_time)
-        # Request one of its inspection spots
-        # print('%s arriving at %d' % (name, self.env.now))
         with self.bcs.request() as req:
             yield req
-            # Charge the battery
-            # print('%s starting to inspect at %s' % (name, self.env.now))
             yield self.env.timeout(inspect_time)
-            # print('%s leaving the bcs at %s' % (name, self.env.now))
             self.inspection_times.append(self.env.now - arriving_time)
 
-    def build_simulation(self, max_time):
+    def build_simulation_by_time(self, max_time, l, mean, sd):
         time = 0
-        count = 0
+        count = 1
         while True:
-            exp_time = exponential(self.l)
-            norm_time = normal(self.mean, self.sd)
+            exp_time = exponential(l)
+            norm_time = normal(mean, sd)
             if time + exp_time + norm_time > max_time:
                 break
-            self.env.process(self.piece('Piece %d' %
-                                        count, time + exp_time, norm_time))
+            self.env.process(self.piece(f'Piece {count}', time + exp_time, norm_time))
+            time += exp_time
+            count += 1
+
+    def build_simulation_by_pieces(self, max_pieces, l, mean, sd):
+        time = 0
+        count = 1
+        while count <= max_pieces:
+            exp_time = exponential(l)
+            norm_time = normal(mean, sd)
+            self.env.process(self.piece(f'Piece {count}', time + exp_time, norm_time))
             time += exp_time
             count += 1
 
@@ -42,20 +43,21 @@ class ProductionLine:
 def main():
     servers = 1
     max_time = 10000
+    max_pieces = 1200
     env = simpy.Environment()
     bcs = simpy.Resource(env, capacity=servers)
-    p_line = ProductionLine(env, bcs, 5, 4, 0.5)
-    p_line.build_simulation(max_time=max_time)
+    p_line = ProductionLine(env, bcs)
+    # p_line.build_simulation_by_time(max_time, 5, 4, 0.5)
+    p_line.build_simulation_by_pieces(max_pieces, 5, 4, 0.5)
     env.run()
 
     times = p_line.inspection_times
-    x = range(len(times))
-    y = [sum(times[:index + 1]) / (index + 1) for index in range(len(times))]
+    y = [average(times[:index + 1]) for index in range(len(times))]
 
     plt.title("Simulation behavior")
     plt.xlabel("Piece number")
     plt.ylabel("Average time in inspection")
-    plt.plot(x, y, color="red")
+    plt.plot(y, color="red")
     plt.legend(
         [f"servers = {servers}, max time = {max_time}"], loc="upper right")
     txt = 'graphs/' + str(servers) + '_servers.png' if servers > 1 else 'graphs/' + str(servers) + '_server.png'
